aa119b9ed059ca4388715fe3c47ad7ee44c2a8d4
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-11-26 10:55:26 +0100

    init

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'redis'
+
+configure :production do
+    uri = URI.parse(ENV["REDISGREEN_URL"])
+    $redis = Redis.new(url: ENV["REDISGREEN_URL"], driver: :hiredis)
+end
+
+configure :development, :test do
+    $redis = Redis.new(url: ENV["WERCKER_REDIS_URL"], driver: :hiredis)
+end
+
+configure :development do
+  $redis = Redis.new()
+end
+
+
+get '/' do
+  decepticons = $redis.smembers('decepticons')
+  return decepticons
+end
+
+get '/add' do
+  erb :new
+end
+
+post '/add' do
+  $redis.sadd('decepticons', 'starscream')
+  redirect '/'
+end
